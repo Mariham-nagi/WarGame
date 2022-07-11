7c72c3b185e4eba566b46a8d3202895ea14802ca
Your Name <you@example.com>
2022-07-10 22:52:26 -0400

    commit for GameWar

---
+++ TestCase.js
@@ -0,0 +1,8 @@
+    describe ("myFunction" ,() => {
+        describe("#FullDeck", () => {
+            it('Should shuffle the whole Cards into 26 cards to each player ', () => {
+                const game = FullDeck()
+                expect (Player1 != Player2)
+        });
+    });
+})
